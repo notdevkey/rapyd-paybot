3a444221499ef1884f1e4de188092db7b4a336b5
Devkey <ronalds.palacis@gmail.com>
2022-11-20 12:46:38 +0200

    feat: simple request payment event

--- apps/discordbot/src/commands/requestPayment.ts
+++ apps/discordbot/src/commands/requestPayment.ts
@@ -1,35 +1,43 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
-import { Command } from "../interfaces/Command";
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { Command } from '../interfaces/Command';
 
 export const requestPayment: Command = {
   data: new SlashCommandBuilder()
-    .setName("request-payment")
-    .setDescription("Request money from a user")
+    .setName('request-payment')
+    .setDescription('Request money from a user')
     .addUserOption((option) =>
       option
-        .setName("user")
-        .setDescription("User to request money from")
+        .setName('user')
+        .setDescription('User to request money from')
         .setRequired(true)
     )
     .addIntegerOption((option) =>
       option
-        .setName("amount")
-        .setDescription("The amount you want to send")
+        .setName('amount')
+        .setDescription('The amount you want to send')
         .setRequired(true)
     )
     .addStringOption((option) =>
       option
-        .setName("message")
-        .setDescription("The message you want to send along the request")
+        .setName('message')
+        .setDescription('The message you want to send along the request')
     ),
   run: async (interaction) => {
     await interaction.deferReply();
     const { user } = interaction;
-    const text = interaction.options.get("message", true);
+    const amount = interaction.options.get('amount', true).value;
+    const message = interaction.options.get('message')?.value;
 
     const paymentRequestEmbed = new EmbedBuilder();
-    paymentRequestEmbed.setTitle("Payment request");
-    paymentRequestEmbed.setDescription(text.message?.content || "");
+    paymentRequestEmbed.setTitle('Payment request');
+    if (message) {
+      paymentRequestEmbed.setDescription(message.toString());
+    }
+    if (amount) {
+      paymentRequestEmbed.setFields([
+        { name: 'Amount', value: amount.toString() },
+      ]);
+    }
     paymentRequestEmbed.setAuthor({
       name: user.tag,
       iconURL: user.displayAvatarURL(),
