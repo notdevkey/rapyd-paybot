445a61d338f9e424bc4debfe17afe535c119d0fd
liskovich <ricardsliskovskis@gmail.com>
2022-11-20 12:54:56 +0200

    feat: wallet service basic operations

---
+++ apps/discordbot/src/service/models/customerCreate.ts
@@ -0,0 +1,6 @@
+export interface CustomerCreate {
+  ewallet_reference_id: string;
+  phone_number: string;
+  email: string;
+  first_name: string;
+}
--- apps/discordbot/src/service/walletService.ts
+++ apps/discordbot/src/service/walletService.ts
@@ -1,12 +1,135 @@
-import { EWalletCreate } from "./models/walletCreate";
+import { EWalletCreate, Contact } from "./models/walletCreate";
 import { EWallet } from "./models/wallet";
+import { CustomerCreate } from "./models/customerCreate";
+import { ErrorResponse } from "./models/error";
+import dotenv from 'dotenv';
+import axios from "axios";
 
-export const createEWallet = async (walletToCreate: EWalletCreate): Promise<EWallet | null> => {
+// TODO: extraxt to env
+const uri = 'https://sandboxapi.rapyd.net/v1/user';
+const access_key = '4C15ED203D6CA6141CF8';
+const salt = 'b142ee4dcf99085d5507091a';
+const timestamp = '1668855267';
+const signature = 'NWZlYjA3MGRkYTM4MTFmNDNmMjk5NGE5Yjg5M2ZiMzQ5ZThhMDcxZjhkY2IxMzNjZGMwMjJiNzJmZWI3OGNiZg==';
+
+const instance = axios.create({
+  baseURL: uri,
+});
+
+export const createEWallet = async (customerData: CustomerCreate): Promise<EWallet | ErrorResponse> => {
   // TODO: implement
-  return null;
+  const walletContact: Contact = {
+    phone_number: customerData.phone_number,
+    email: customerData.email,
+    first_name: customerData.first_name,
+    contact_type: "personal",
+  };
+  
+  const walletToCreate: EWalletCreate = {
+    ewallet_reference_id: customerData.ewallet_reference_id,
+    type: "person",
+    contact: walletContact
+  };
+
+  // TODO: extract to some common place
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json',
+      'access_key': access_key,
+      'salt': salt,
+      'timestamp': timestamp,
+      'signature': signature,
+    },
+  };
+
+  try {
+    const response = await instance.post<EWallet>(uri, walletToCreate, config);
+
+    // TODO: remove log
+    console.log(response.data)
+    return response.data;
+  } catch(e) {
+    const err: ErrorResponse = {
+      msg: ''
+    };
+    if (axios.isAxiosError(e)) {
+      err.msg = e.message;
+    } else {
+      err.msg = "Unexpected Error occurerd";
+    }
+
+    // TODO: remove log
+    console.log(err.msg);
+    return err;
+  }
 };
 
-export const retrieveEWallet = async (walletId: string): Promise<EWallet | null> => {
+export const retrieveEWallet = async (walletId: string): Promise<EWallet | ErrorResponse> => {
   // TODO: implement
-  return null;
+
+  // TODO: extract to some common place
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json',
+      'access_key': access_key,
+      'salt': salt,
+      'timestamp': timestamp,
+      'signature': signature,
+    },
+  };
+
+  try {
+    const response = await instance.get<EWallet>(`${uri}/${walletId}`, config);
+
+    // TODO: remove log
+    console.log(response.data)
+    return response.data;
+  } catch (e) {
+    const err: ErrorResponse = {
+      msg: ''
+    };
+    if (axios.isAxiosError(e)) {
+      err.msg = e.message;
+    } else {
+      err.msg = "Unexpected Error occurerd";
+    }
+
+    // TODO: remove log
+    console.log(err.msg);
+    return err;
+  }
 };
+
+export const updateEWallet = async (eWallet: EWallet): Promise<EWallet | ErrorResponse> => {
+  // TODO: extract to some common place
+  const config = {
+    headers: { 
+      'Content-Type': 'application/json',
+      'access_key': access_key,
+      'salt': salt,
+      'timestamp': timestamp,
+      'signature': signature,
+    },
+  };
+
+  try {
+    const response = await instance.put<EWallet>(uri, eWallet, config);
+
+    // TODO: remove log
+    console.log(response.data)
+    return response.data;
+  } catch (e) {
+    const err: ErrorResponse = {
+      msg: ''
+    };
+    if (axios.isAxiosError(e)) {
+      err.msg = e.message;
+    } else {
+      err.msg = "Unexpected Error occurerd";
+    }
+
+    // TODO: remove log
+    console.log(err.msg);
+    return err;
+  }
+}
