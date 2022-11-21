528daf78585a8def45d90681a8b250a526ac5aae
liskovich <ricardsliskovskis@gmail.com>
2022-11-21 23:17:13 +0200

    fix: resolve wrong timestamp for request header

--- apps/discordbot/src/service/authService.ts
+++ apps/discordbot/src/service/authService.ts
@@ -1,14 +1,24 @@
 import * as crypto from "crypto-js";
 
 const salt = crypto.lib.WordArray.random(12);
-const timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString();
+const idempotency = new Date().getTime().toString();
+//const timestamp = (Math.floor(new Date().getTime() / 1000) - 10).toString();
+const timestamp = Math.round(new Date().getTime() / 1000).toString();
 const accessKey: string = process.env.ACCESS_KEY!;
 const secretKey: string = process.env.SECRET_KEY!;
 
 const getSignature = (method: string, uri: string, data: any): string => {
-  const requestData = JSON.stringify(data).trim();
-  const toSign = method + uri + salt + timestamp + accessKey + secretKey + requestData;
-  
+  let requestData = JSON.stringify(data).trim();
+  requestData = requestData == '{}' ? '' : requestData;
+
+  const toSign = method.toLowerCase() + uri + salt + timestamp + accessKey + secretKey + requestData;
+
+  /**
+    let hash = crypto.createHmac('sha256', secretKey);
+    hash.update(toSign);
+    const signature = Buffer.from(hash.digest("hex")).toString("base64")
+  */
+
   let signature: string = crypto.enc.Hex.stringify(
     crypto.HmacSHA256(toSign, secretKey)
   );
