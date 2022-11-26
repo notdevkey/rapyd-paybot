ff06d97d537c29d51b82061ba0c7590ccd7b19af
liskovich <ricardsliskovskis@gmail.com>
2022-11-26 22:30:00 +0200

    feat: connect ds command with payment logic

--- apps/discordbot/src/commands/requestPayment.ts
+++ apps/discordbot/src/commands/requestPayment.ts
@@ -1,5 +1,9 @@
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { APIEmbedField, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { makeTransaction } from '../service/paymentService';
 import { Command } from '../interfaces/Command';
+import { PaymentCreate } from '../service/models/paymentCreate';
+import { Transaction } from "../service/models/payment";
+import { ErrorResponse } from '../service/models/error';
 
 export const requestPayment: Command = {
   data: new SlashCommandBuilder()
@@ -25,18 +29,62 @@ export const requestPayment: Command = {
   run: async (interaction) => {
     await interaction.deferReply();
     const { user } = interaction;
-    const amount = interaction.options.get('amount', true).value;
-    const message = interaction.options.get('message')?.value;
+    const amount = interaction.options.get('amount', true).value as number;
+    const paymentMessage = interaction.options.get('message')?.value as string;
+
+    const receiver = interaction.options.getUser('user');
+
+    let message: APIEmbedField | APIEmbedField[] = [];
+    let title: string = '';
+    let description: string = '';
+    if (!receiver) {
+      // TODO: show no account error
+      message = [
+        { name: 'Error', value: 'The receiver does not have an account' }
+      ];
+      title = 'Transaction Failed';
+      description = `Hey, ${user.username}! The receiver of your transaction does not have an account!`;
+      return;
+    } 
+    // TODO: get user's and receiver's ewallets here
+    
+    // TODO: make transaction   
+    const transaction: PaymentCreate = {
+      sender: user.tag,
+      amount: amount,
+      receiver: receiver!.tag,
+      message: paymentMessage
+    }
+    const paymentCreated = await makeTransaction(transaction);
+    // TODO: check if payment success
+    // TODO: only for test purposes, remove sensitive data later
+    if ((paymentCreated as Transaction).data != null) {
+      const result = (paymentCreated as Transaction).data;
+      message = [
+        { name: 'Sender wallet', value: result.sourceEwalletID },
+        { name: 'Receiver wallet', value: result.destinationEwalletID },
+        { name: 'Transaction ID', value: result.id },
+        { name: 'Ammount', value: result.amount.toString() },
+        { name: 'Currency', value: result.currencyCode },
+        { name: 'Transaction Status', value: result.status },
+      ];
+      title = 'Transaction successful!';
+      description = `Hey, ${user.username}! The money has been successfully sent to ${receiver.username}!`;
+    } else if ((paymentCreated as ErrorResponse).msg != null) {
+      message = [
+        { name: 'Error', value: (paymentCreated as ErrorResponse).msg }
+      ];
+      title = 'Transactiond Failed';
+      description = `Hey, ${user.username}! Some error occured when creating Transaction to ${receiver.username}!`;
+    }
 
     const paymentRequestEmbed = new EmbedBuilder();
-    paymentRequestEmbed.setTitle('Payment request');
+    paymentRequestEmbed.setTitle(title);
     if (message) {
-      paymentRequestEmbed.setDescription(message.toString());
+      paymentRequestEmbed.setDescription(description);
     }
     if (amount) {
-      paymentRequestEmbed.setFields([
-        { name: 'Amount', value: amount.toString() },
-      ]);
+      paymentRequestEmbed.setFields(message);
     }
     paymentRequestEmbed.setAuthor({
       name: user.tag,
