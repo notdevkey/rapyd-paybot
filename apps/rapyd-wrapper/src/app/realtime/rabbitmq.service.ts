615b7cddc63004bddb8ed49f5d62491f0fd4bac8
liskovich <ricardsliskovskis@gmail.com>
2022-12-08 17:18:17 +0200

    feat: push to Q functionality

--- apps/rapyd-wrapper/src/app/realtime/rabbitmq.service.ts
+++ apps/rapyd-wrapper/src/app/realtime/rabbitmq.service.ts
@@ -9,23 +9,32 @@ class RabbitMQService {
     const username = process.env.RABBITMQ_USER;
     const password = process.env.RABBITMQ_PASS;
 
-    amqp.connect(`amqp://${username}:${password}@localhost:5672`, (err0, connection) => {
-      if (err0) {
-        throw err0;
-      }
-
-      connection.createChannel((err1, channel) => {
-        if (err1) {
-          throw err1;
+    amqp.connect(
+      `amqp://${username}:${password}@localhost:5672`,
+      (err0, connection) => {
+        if (err0) {
+          throw err0;
         }
 
-        this._channel = channel;
-        //
-      });
-    });
+        connection.createChannel((err1, channel) => {
+          if (err1) {
+            throw err1;
+          }
+
+          this._channel = channel;
+        });
+
+        process.on('beforeExit', () => {
+          console.log('closed connection');
+          connection.close();
+        });
+      }
+    );
   }
 
-  //
+  public sendToQueue = <T>(queueName: string, msg: T) => {
+    this._channel.sendToQueue(queueName, Buffer.from(JSON.stringify(msg)));
+  };
 }
 
 export default RabbitMQService;
